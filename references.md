9fbd5abce821d2ebe11ca69e3192d30f02b8f711
Chris Henson <chrishenson.net@gmail.com>
2024-05-04 19:11:35 -0400

    refs

---
+++ references.md
@@ -0,0 +1,10 @@
+---
+title: References
+---
+
+## References
+
+I maintain a [public Zotero
+library](https://www.zotero.org/chenson2018/library) of my infinitely expanding
+reading list. The library has tags (bottom-left corner) which can be used to
+filter items.
--- site.hs
+++ site.hs
@@ -44,7 +44,7 @@ main = hakyll $ do
         route   idRoute
         compile compressCssCompiler
 
-    match "about.md" $ do
+    match (fromList ["about.md", "references.md"]) $ do
         route   $ setExtension "html"
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
